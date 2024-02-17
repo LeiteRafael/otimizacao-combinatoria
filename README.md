027a180271ae61d6f6f85174e3798ed54266c257
Rafael Leite <43257954+LeiteRafael@users.noreply.github.com>
2024-02-16 22:48:32 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# otimizacao-combinatoria
+# Lista de programação linear
+
+Para executar os exercicios 
+```bash
+node exercicioN.js
+```
+onde N é numero do exercicio
+
+Para instalar os pacotes necessarios 
+```bash
+npm install
+```
--- exercicio04.js
+++ exercicio04.js
@@ -4,7 +4,7 @@ class Exercicio04 {
     // Construtor para usar as abstrações do glpkAdapter
     constructor() {
         this._nameOfProblem = "Um problema de estoque";
-        this._descriptionOfZ = "-----"
+        this._descriptionOfZ = "Custo minimo de transporte para atender todos os clientes"
         this._resolveProblem = glpkAdapter.resolveProblem;
         this._createInputData = glpkAdapter.createInputData;
         this._writeFormulation = glpkAdapter.writeFormulation;
@@ -13,17 +13,14 @@ class Exercicio04 {
 
     execute() { // Chamada principal do programa
 
-        const numOfCustormers = 4;
-        const numOfDeposits = 2;
+        const numOfCustormers = 5;
+        const numOfDeposits = 5;
 
-        const { transportationCost, stock, customerDemands } = generateRandomValues(numOfDeposits, numOfCustormers);
+        const { transportationCost, stock, customerDemands } = this._generateRandomValues(numOfDeposits, numOfCustormers);
 
         const vars = this._createVars(numOfDeposits, numOfCustormers, transportationCost);
 
-        console.log(stock);
-        const constraints = this._mountConstraints(vars, stock, customerDemands);
-
-        console.log(constraints);
+        const constraints = this._mountConstraints(vars, stock, customerDemands, numOfCustormers, numOfDeposits);
 
         const input = this._createInputData(vars, constraints, this._nameOfProblem, 'MIN');
 
@@ -31,15 +28,15 @@ class Exercicio04 {
         const computation = this._resolveProblem(input, this.options, this._descriptionOfZ);
         console.log("----- ", computation.result.vars, '\n\n');
 
+        console.log("array dos custo de transporte", transportationCost, "\n");
+        console.log("array dos estoques", stock, "\n");
+        console.log("array das demandas", customerDemands, "\n");
     }
 
-    // 
-
-    _createVars(numOfDeposits, numOfCustormers, transportationCost,) {
+    _createVars(numOfDeposits, numOfCustormers, transportationCost,) { //cria as variaveis numDepositos x numOfCustomers
         const vars = [];
         for (let i = 0; i < numOfDeposits; i++) {
             for (let j = 0; j < numOfCustormers; j++) {
-                console.log(transportationCost[i][j]);
                 vars.push({ name: `qtd_deposito_${i + 1}_cliente_${j + 1}`, coef: transportationCost[i][j] });
             }
         }
@@ -47,12 +44,8 @@ class Exercicio04 {
         return vars;
     }
 
-    _mountConstraints(vars, stock, customerDemands) { // Aqui trouxe a restrições da direita para a esquerda e inverti o sinal
+    _mountConstraints(vars, stock, customerDemands, numOfCustormers, numOfDeposits) {
         const constraints = [];
-        const numOfCustormers = 4;
-        const numOfDeposits = 2;
-
-        console.log(customerDemands);
 
         let count = 0;
         let limit = 0;
@@ -60,68 +53,59 @@ class Exercicio04 {
             let varsDeposito = [];
 
             for (let j = 0; j < numOfCustormers; j++) {
-                console.log("aquiiiii", vars[count].name);
                 varsDeposito.push([1, vars[count].name]);
                 count++;
             }
-
             constraints[i] = this._createConstraints(varsDeposito, '<=', stock["deposit_" + i]);
             limit = i;
         }
 
+        const aux = [];
+        vars.forEach(element => aux.push(element.name));
 
-
-        count = 0;
         for (let i = 1; i <= numOfCustormers; i++) {
             let varsDeposito = [];
 
-            for (let j = 0; j < numOfDeposits; j++) {
-
-                vars.forEach(element => {
-                    if (element.name.endsWith('cliente_' + j)) {
-                        varsDeposito.push([1, element.name]);
-                    }
-                });
-                count++;
+            for (let j = 1; j <= numOfDeposits; j++) {
+                ;
+                varsDeposito.push([1, aux[aux.indexOf('qtd_deposito_' + j + '_cliente_' + i)]]);
             }
-
-            constraints[limit + i] = this._createConstraints(varsDeposito, '=', customerDemands["customer_" + i]);
+            constraints[i + limit] = this._createConstraints(varsDeposito, '=', customerDemands["customer_" + i]);
         }
 
         return constraints;
+    }
+
+    _generateRandomValues(numOfDeposits, numOfCustormers) {
+        const depositArray = Array.from({ length: numOfDeposits }, (_, i) => "deposit_" + (i + 1));
+        const customerArray = Array.from({ length: numOfCustormers }, (_, i) => "customer_" + (i + 1));
+
+        const stock = {};
+        const customerDemands = {};
+        const transportationCost = [];
+
+        depositArray.forEach(deposit => {
+            stock[deposit] = Math.floor(Math.random() * 100) + 1;
+        });
+
+        customerArray.forEach(customer => {
+            customerDemands[customer] = Math.floor(Math.random() * 50) + 1;
+        });
+
+        for (let i = 0; i < depositArray.length; i++) {
+            transportationCost[i] = [];
+            for (let j = 0; j < customerArray.length; j++) {
+                transportationCost[i][j] = Math.floor(Math.random() * 10) + 1;
+            }
+        }
+
+        return {
+            stock,
+            customerDemands,
+            transportationCost
+        };
     }
 }
 
 const exercicio4 = new Exercicio04();
 exercicio4.execute();
-
-
-function generateRandomValues(numOfDeposits, numOfCustormers) {
-    const depositArray = Array.from({ length: numOfDeposits }, (_, i) => "deposit_" + (i + 1));
-    const customerArray = Array.from({ length: numOfCustormers }, (_, i) => "customer_" + (i + 1));
-
-    const stock = {};
-    const customerDemands = {};
-    const transportationCost = [];
-
-    depositArray.forEach(deposit => {
-        stock[deposit] = Math.floor(Math.random() * 50) + 1;
-    });
-
-    customerArray.forEach(customer => {
-        customerDemands[customer] = Math.floor(Math.random() * 50) + 1;
-    });
-
-    for (let i = 0; i < depositArray.length; i++) {
-        transportationCost[i] = [];
-        for (let j = 0; j < customerArray.length; j++) {
-            transportationCost[i][j] = Math.floor(Math.random() * 10) + 1;
-        }
-    }
-
-    return {
-        stock,
-        customerDemands,
-        transportationCost
-    };
-}
